1f5a7d07e49bcce21df5b5dbbde7a6471d6e1f41
Daniel Rodriguez <daniel.rodriguez.iglesias9@gmail.com>
2020-05-01 13:33:17 +0200

    2 commit rama main

--- DanielRodriguez.java
+++ DanielRodriguez.java
@@ -3,6 +3,7 @@ public class DanielRodriguez {
 	private String tel;
 	DanielRodriguez (int e, String t)  {     
 		edat=e;     
-		tel=t;       
+		tel=t;
+		System.out.println('No alargaré más el código java, esta es una práctica de git');
 	}
 }
